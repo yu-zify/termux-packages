siddharthsky/CustTermux

wlroots: implement Termux:GUI backend

x11-packages/wlroots/src/backend/termuxgui/backend.c
@@ -1,0 +1,178 @@
+#include <assert.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <sys/eventfd.h>
+#include <unistd.h>
+
+#include "backend/termuxgui.h"
+
+struct wlr_tgui_backend *tgui_backend_from_backend(struct wlr_backend *wlr_backend) {
+    assert(wlr_backend_is_tgui(wlr_backend));
+    return (struct wlr_tgui_backend *) wlr_backend;
+}
+
+static bool backend_start(struct wlr_backend *wlr_backend) {
+    struct wlr_tgui_backend *backend = tgui_backend_from_backend(wlr_backend);
+    backend->started = true;
+    wlr_log(WLR_INFO, "Starting Termux:GUI backend");
+
+    wl_signal_emit_mutable(&backend->backend.events.new_input, &backend->keyboard.base);
+    wl_signal_emit_mutable(&backend->backend.events.new_input, &backend->pointer.base);
+
+    for (uint32_t i = 0; i < backend->requested_outputs; i++) {
+        wlr_tgui_output_create(&backend->backend);
+    }
+    return true;
+}
+
+static void backend_destroy(struct wlr_backend *wlr_backend) {
+    struct wlr_tgui_backend *backend = tgui_backend_from_backend(wlr_backend);
+    if (!wlr_backend) {
+        return;
+    }
+
+    wl_list_remove(&backend->display_destroy.link);
+    wl_event_source_remove(backend->tgui_event_source);
+
+    struct wlr_tgui_output *output, *output_tmp;
+    wl_list_for_each_safe(output, output_tmp, &backend->outputs, link) {
+        wlr_output_destroy(&output->wlr_output);
+    }
+
+    wlr_allocator_destroy(backend->allocator);
+    wlr_pointer_finish(&backend->pointer);
+    wlr_keyboard_finish(&backend->keyboard);
+    wlr_backend_finish(wlr_backend);
+
+    tgui_connection_destroy(backend->conn);
+    pthread_join(backend->tgui_event_thread, NULL);
+    wlr_queue_destroy(&backend->event_queue);
+
+    close(backend->tgui_event_fd);
+    free(backend);
+}
+
+static uint32_t get_buffer_caps(struct wlr_backend *wlr_backend) {
+    return WLR_BUFFER_CAP_DATA_PTR | WLR_BUFFER_CAP_DMABUF;
+}
+
+static const struct wlr_backend_impl backend_impl = {
+    .start = backend_start,
+    .destroy = backend_destroy,
+    .get_buffer_caps = get_buffer_caps,
+};
+
+static void handle_display_destroy(struct wl_listener *listener, void *data) {
+    struct wlr_tgui_backend *backend = wl_container_of(listener, backend, display_destroy);
+    backend_destroy(&backend->backend);
+}
+
+static int handle_tgui_event(int fd, uint32_t mask, void *data) {
+    struct wlr_tgui_backend *backend = data;
+
+    if ((mask & WL_EVENT_HANGUP) || (mask & WL_EVENT_ERROR)) {
+        if (mask & WL_EVENT_ERROR) {
+            wlr_log(WLR_ERROR, "Failed to read from tgui event");
+            wlr_backend_destroy(&backend->backend);
+        }
+        return 0;
+    }
+
+    eventfd_t event_count = 0;
+    if (eventfd_read(backend->tgui_event_fd, &event_count) < 0) {
+        return 0;
+    }
+
+    struct wl_list *elm = wlr_queue_pull(&backend->event_queue, true);
+    if (elm == NULL) {
+        wlr_log(WLR_ERROR, "tgui event queue is empty");
+        return 0;
+    }
+    struct wlr_tgui_event *event = wl_container_of(elm, event, link);
+
+    struct wlr_tgui_output *output, *output_tmp;
+    wl_list_for_each_safe(output, output_tmp, &backend->outputs, link) {
+        if (event->e.activity == output->activity) {
+            handle_activity_event(&event->e, output);
+        }
+    }
+    tgui_event_destroy(&event->e);
+    free(event);
+
+    return 0;
+}
+
+static void *tgui_event_thread(void *data) {
+    struct wlr_tgui_backend *backend = data;
+
+    tgui_event event;
+    while (tgui_wait_event(backend->conn, &event) == TGUI_ERR_OK) {
+        struct wlr_tgui_event *wlr_event = calloc(1, sizeof(*wlr_event));
+        if (wlr_event) {
+            memcpy(&wlr_event->e, &event, sizeof(tgui_event));
+
+            wlr_queue_push(&backend->event_queue, &wlr_event->link);
+
+            eventfd_write(backend->tgui_event_fd, 1);
+        } else {
+            wlr_log(WLR_ERROR, "tgui event loss: out of memory");
+            tgui_event_destroy(&event);
+        }
+    }
+
+    return 0;
+}
+
+const struct wlr_pointer_impl tgui_pointer_impl = {
+    .name = "tgui-pointer",
+};
+
+const struct wlr_keyboard_impl tgui_keyboard_impl = {
+    .name = "tgui-keyboard",
+};
+
+struct wlr_backend *wlr_tgui_backend_create(struct wl_display *display) {
+    wlr_log(WLR_INFO, "Creating Termux:GUI backend");
+
+    struct wlr_tgui_backend *backend = calloc(1, sizeof(*backend));
+    if (!backend) {
+        wlr_log(WLR_ERROR, "Failed to allocate wlr_tgui_backend");
+        return NULL;
+    }
+    wlr_backend_init(&backend->backend, &backend_impl);
+
+    backend->display = display;
+    backend->loop = wl_display_get_event_loop(display);
+    backend->tgui_event_fd = eventfd(0, EFD_CLOEXEC | EFD_NONBLOCK | EFD_SEMAPHORE);
+
+    if (tgui_connection_create(&backend->conn)) {
+        wlr_log(WLR_ERROR, "Failed to create tgui_connection");
+        wlr_backend_finish(&backend->backend);
+        free(backend);
+        return NULL;
+    }
+    backend->allocator = wlr_tgui_allocator_create(backend);
+
+    wlr_pointer_init(&backend->pointer, &tgui_pointer_impl, "tgui-pointer");
+    wlr_keyboard_init(&backend->keyboard, &tgui_keyboard_impl, "tgui-keyboard");
+
+    wl_list_init(&backend->outputs);
+
+    backend->display_destroy.notify = handle_display_destroy;
+    wl_display_add_destroy_listener(display, &backend->display_destroy);
+
+    uint32_t events = WL_EVENT_READABLE | WL_EVENT_ERROR | WL_EVENT_HANGUP;
+    backend->tgui_event_source = wl_event_loop_add_fd(backend->loop, backend->tgui_event_fd,
+                                                      events, handle_tgui_event, backend);
+
+    wlr_queue_init(&backend->event_queue);
+    pthread_create(&backend->tgui_event_thread, NULL, tgui_event_thread, backend);
+
+    return &backend->backend;
+}
+
+struct wlr_allocator *wlr_tgui_backend_get_allocator(struct wlr_tgui_backend *backend) {
+    return backend->allocator;
+}
+
+bool wlr_backend_is_tgui(struct wlr_backend *backend) { return backend->impl == &backend_impl; }

x11-packages/wlroots/src/include/backend/termuxgui.h
@@ -1,0 +1,211 @@
+#ifndef BACKEND_TERMUXGUI_H
+#define BACKEND_TERMUXGUI_H
+
+#include <android/hardware_buffer.h>
+#include <assert.h>
+#include <pthread.h>
+#include <termuxgui/termuxgui.h>
+
+#include <wlr/backend/interface.h>
+#include <wlr/backend/termuxgui.h>
+#include <wlr/interfaces/wlr_buffer.h>
+#include <wlr/interfaces/wlr_keyboard.h>
+#include <wlr/interfaces/wlr_output.h>
+#include <wlr/interfaces/wlr_pointer.h>
+#include <wlr/render/allocator.h>
+#include <wlr/types/wlr_output_layer.h>
+#include <wlr/util/log.h>
+
+#define DEFAULT_REFRESH (60 * 1000) // 60 Hz
+
+#define TRY_LOG(func, ...)                                                                       \
+    do {                                                                                         \
+        tgui_err ret = func(__VA_ARGS__);                                                        \
+        if (ret != TGUI_ERR_OK) {                                                                \
+            wlr_log(WLR_ERROR, #func " failed: %s", TGUI_ERR_TO_STR(ret));                       \
+        }                                                                                        \
+    } while (0)
+
+static const char *TGUI_ERR_STR[] = {
+    [TGUI_ERR_SYSTEM] = "TGUI_ERR_SYSTEM",
+    [TGUI_ERR_CONNECTION_LOST] = "TGUI_ERR_CONNECTION_LOST",
+    [TGUI_ERR_ACTIVITY_DESTROYED] = "TGUI_ERR_ACTIVITY_DESTROYED",
+    [TGUI_ERR_MESSAGE] = "TGUI_ERR_MESSAGE",
+    [TGUI_ERR_NOMEM] = "TGUI_ERR_NOMEM",
+    [TGUI_ERR_EXCEPTION] = "TGUI_ERR_EXCEPTION",
+    [TGUI_ERR_VIEW_INVALID] = "TGUI_ERR_VIEW_INVALID",
+    [TGUI_ERR_API_LEVEL] = "TGUI_ERR_API_LEVEL",
+};
+
+static inline const char *TGUI_ERR_TO_STR(tgui_err err) {
+    if (err > TGUI_ERR_API_LEVEL) {
+        return "TGUI_ERR_UNKNOWN";
+    }
+    return TGUI_ERR_STR[err];
+}
+
+typedef struct native_handle {
+    int version; /* sizeof(native_handle_t) */
+    int numFds;  /* number of file-descriptors at &data[0] */
+    int numInts; /* number of ints at &data[numFds] */
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wzero-length-array"
+#endif
+    int data[0]; /* numFds + numInts ints */
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#endif
+} native_handle_t;
+
+struct wlr_queue {
+    struct wl_list base;
+    int length;
+    pthread_cond_t cond;
+    pthread_mutex_t mutex;
+};
+
+static inline void wlr_queue_init(struct wlr_queue *queue) {
+    queue->length = 0;
+    wl_list_init(&queue->base);
+    pthread_cond_init(&queue->cond, NULL);
+    pthread_mutex_init(&queue->mutex, NULL);
+}
+
+static inline void wlr_queue_destroy(struct wlr_queue *queue) {
+    pthread_cond_destroy(&queue->cond);
+    pthread_mutex_destroy(&queue->mutex);
+}
+
+static inline struct wl_list *wlr_queue_pull(struct wlr_queue *queue, bool nonblock) {
+    pthread_mutex_lock(&queue->mutex);
+
+    if (queue->length == 0) {
+        if (nonblock) {
+            pthread_mutex_unlock(&queue->mutex);
+            return NULL;
+        }
+        pthread_cond_wait(&queue->cond, &queue->mutex);
+    }
+    assert(queue->length > 0);
+
+    queue->length--;
+    struct wl_list *elm = queue->base.prev;
+    wl_list_remove(elm);
+
+    pthread_mutex_unlock(&queue->mutex);
+    return elm;
+}
+
+static inline void wlr_queue_push(struct wlr_queue *queue, struct wl_list *elm) {
+    pthread_mutex_lock(&queue->mutex);
+    if (wl_list_empty(&queue->base)) {
+        pthread_cond_signal(&queue->cond);
+    }
+    queue->length++;
+    wl_list_insert(&queue->base, elm);
+    pthread_mutex_unlock(&queue->mutex);
+}
+
+static inline int wlr_queue_length(struct wlr_queue *queue) {
+    pthread_mutex_lock(&queue->mutex);
+    int ret = queue->length;
+    pthread_mutex_unlock(&queue->mutex);
+    return ret;
+}
+
+struct wlr_tgui_backend {
+    struct wlr_backend backend;
+    struct wl_display *display;
+    struct wl_event_loop *loop;
+    struct wlr_allocator *allocator;
+
+    struct wlr_pointer pointer;
+    struct wlr_keyboard keyboard;
+
+    size_t requested_outputs;
+    struct wl_list outputs;
+    struct wl_listener display_destroy;
+    bool started;
+
+    tgui_connection conn;
+    struct wlr_queue event_queue;
+    int tgui_event_fd;
+    pthread_t tgui_event_thread;
+    struct wl_event_source *tgui_event_source;
+};
+
+struct wlr_tgui_allocator {
+    struct wlr_allocator wlr_allocator;
+    tgui_connection conn;
+};
+
+struct wlr_tgui_buffer {
+    struct wlr_buffer wlr_buffer;
+
+    void *data;
+    uint32_t format;
+    tgui_connection conn;
+    tgui_hardware_buffer buffer;
+    AHardwareBuffer_Desc desc;
+    struct wl_list link;
+    struct wlr_dmabuf_attributes dmabuf;
+
+    void *dlhandle;
+    int (*lock)(AHardwareBuffer *buffer,
+                uint64_t usage,
+                int32_t fence,
+                const ARect *rect,
+                void **outVirtualAddress);
+    int (*unlock)(AHardwareBuffer *buffer, int32_t *fence);
+    void (*describe)(const AHardwareBuffer *buffer, AHardwareBuffer_Desc *outDesc);
+    const native_handle_t *(*getNativeHandle)(const AHardwareBuffer *buffer);
+};
+
+struct wlr_tgui_output {
+    struct wlr_output wlr_output;
+
+    struct wlr_tgui_backend *backend;
+    struct wl_list link;
+
+    tgui_activity activity;
+    tgui_view surface;
+    bool foreground;
+
+    struct wlr_queue present_queue;
+    struct wlr_queue idle_queue;
+    bool present_thread_run;
+    pthread_t present_thread;
+    int present_complete_fd;
+    struct wl_event_source *present_complete_source;
+
+    struct {
+        int id, max;
+        double x, y;
+        bool moved, down;
+        uint64_t time_ms;
+    } touch_pointer;
+
+    double cursor_x, cursor_y;
+};
+
+struct wlr_tgui_event {
+    tgui_event e;
+    struct wl_list link;
+};
+
+struct wlr_tgui_backend *tgui_backend_from_backend(struct wlr_backend *wlr_backend);
+
+struct wlr_allocator *wlr_tgui_allocator_create(struct wlr_tgui_backend *backend);
+
+struct wlr_allocator *wlr_tgui_backend_get_allocator(struct wlr_tgui_backend *backend);
+
+struct wlr_tgui_buffer *tgui_buffer_from_buffer(struct wlr_buffer *wlr_buffer);
+
+int handle_activity_event(tgui_event *e, struct wlr_tgui_output *output);
+
+void handle_touch_event(tgui_event *e, struct wlr_tgui_output *output, uint64_t time_ms);
+
+void handle_keyboard_event(tgui_event *e, struct wlr_tgui_output *output, uint64_t time_ms);
+
+#endif

x11-packages/wlroots/src/include/wlr/backend/termuxgui.h
@@ -1,0 +1,31 @@
+/*
+ * This an unstable interface of wlroots. No guarantees are made regarding the
+ * future consistency of this API.
+ */
+#ifndef WLR_USE_UNSTABLE
+#error "Add -DWLR_USE_UNSTABLE to enable unstable wlroots features"
+#endif
+
+#ifndef WLR_BACKEND_TERMUXGUI_H
+#define WLR_BACKEND_TERMUXGUI_H
+
+#include <wlr/backend.h>
+#include <wlr/types/wlr_output.h>
+
+/**
+ * Creates a Termux:GUI backend, and connection to the Termux:GUI plugin.
+ * A Termux:GUI backend has no outputs or inputs by default.
+ */
+struct wlr_backend *wlr_tgui_backend_create(struct wl_display *display);
+/**
+ * Create a new Termux:GUI output.
+ *
+ * Will use Termux:GUI plugin to create Activity and SurfaceView, the buffers presented
+ * on the output is displayed to the SurfaceView.
+ */
+struct wlr_output *wlr_tgui_output_create(struct wlr_backend *backend);
+
+bool wlr_backend_is_tgui(struct wlr_backend *backend);
+bool wlr_output_is_tgui(struct wlr_output *output);
+
+#endif

yu-zify/termux-packages

x11-packages/wlroots/src/backend/termuxgui/backend.c
@@ -1,0 +1,177 @@
+#include <assert.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <sys/eventfd.h>
+#include <unistd.h>
+
+#include "backend/termuxgui.h"
+
+struct wlr_tgui_backend *tgui_backend_from_backend(struct wlr_backend *wlr_backend) {
+    assert(wlr_backend_is_tgui(wlr_backend));
+    return (struct wlr_tgui_backend *) wlr_backend;
+}
+
+static bool backend_start(struct wlr_backend *wlr_backend) {
+    struct wlr_tgui_backend *backend = tgui_backend_from_backend(wlr_backend);
+    backend->started = true;
+    wlr_log(WLR_INFO, "Starting Termux:GUI backend");
+
+    wl_signal_emit_mutable(&backend->backend.events.new_input, &backend->keyboard.base);
+    wl_signal_emit_mutable(&backend->backend.events.new_input, &backend->pointer.base);
+
+    for (uint32_t i = 0; i < backend->requested_outputs; i++) {
+        wlr_tgui_output_create(&backend->backend);
+    }
+    return true;
+}
+
+static void backend_destroy(struct wlr_backend *wlr_backend) {
+    struct wlr_tgui_backend *backend = tgui_backend_from_backend(wlr_backend);
+    if (!wlr_backend) {
+        return;
+    }
+
+    wl_list_remove(&backend->event_loop_destroy.link);
+    wl_event_source_remove(backend->tgui_event_source);
+
+    struct wlr_tgui_output *output, *output_tmp;
+    wl_list_for_each_safe(output, output_tmp, &backend->outputs, link) {
+        wlr_output_destroy(&output->wlr_output);
+    }
+
+    wlr_allocator_destroy(backend->allocator);
+    wlr_pointer_finish(&backend->pointer);
+    wlr_keyboard_finish(&backend->keyboard);
+    wlr_backend_finish(wlr_backend);
+
+    tgui_connection_destroy(backend->conn);
+    pthread_join(backend->tgui_event_thread, NULL);
+    wlr_queue_destroy(&backend->event_queue);
+
+    close(backend->tgui_event_fd);
+    free(backend);
+}
+
+static uint32_t get_buffer_caps(struct wlr_backend *wlr_backend) {
+    return WLR_BUFFER_CAP_DATA_PTR | WLR_BUFFER_CAP_DMABUF;
+}
+
+static const struct wlr_backend_impl backend_impl = {
+    .start = backend_start,
+    .destroy = backend_destroy,
+    .get_buffer_caps = get_buffer_caps,
+};
+
+static void handle_event_loop_destroy(struct wl_listener *listener, void *data) {
+    struct wlr_tgui_backend *backend = wl_container_of(listener, backend, event_loop_destroy);
+    backend_destroy(&backend->backend);
+}
+
+static int handle_tgui_event(int fd, uint32_t mask, void *data) {
+    struct wlr_tgui_backend *backend = data;
+
+    if ((mask & WL_EVENT_HANGUP) || (mask & WL_EVENT_ERROR)) {
+        if (mask & WL_EVENT_ERROR) {
+            wlr_log(WLR_ERROR, "Failed to read from tgui event");
+            wlr_backend_destroy(&backend->backend);
+        }
+        return 0;
+    }
+
+    eventfd_t event_count = 0;
+    if (eventfd_read(backend->tgui_event_fd, &event_count) < 0) {
+        return 0;
+    }
+
+    struct wl_list *elm = wlr_queue_pull(&backend->event_queue, true);
+    if (elm == NULL) {
+        wlr_log(WLR_ERROR, "tgui event queue is empty");
+        return 0;
+    }
+    struct wlr_tgui_event *event = wl_container_of(elm, event, link);
+
+    struct wlr_tgui_output *output, *output_tmp;
+    wl_list_for_each_safe(output, output_tmp, &backend->outputs, link) {
+        if (event->e.activity == output->activity) {
+            handle_activity_event(&event->e, output);
+        }
+    }
+    tgui_event_destroy(&event->e);
+    free(event);
+
+    return 0;
+}
+
+static void *tgui_event_thread(void *data) {
+    struct wlr_tgui_backend *backend = data;
+
+    tgui_event event;
+    while (tgui_wait_event(backend->conn, &event) == TGUI_ERR_OK) {
+        struct wlr_tgui_event *wlr_event = calloc(1, sizeof(*wlr_event));
+        if (wlr_event) {
+            memcpy(&wlr_event->e, &event, sizeof(tgui_event));
+
+            wlr_queue_push(&backend->event_queue, &wlr_event->link);
+
+            eventfd_write(backend->tgui_event_fd, 1);
+        } else {
+            wlr_log(WLR_ERROR, "tgui event loss: out of memory");
+            tgui_event_destroy(&event);
+        }
+    }
+
+    return 0;
+}
+
+const struct wlr_pointer_impl tgui_pointer_impl = {
+    .name = "tgui-pointer",
+};
+
+const struct wlr_keyboard_impl tgui_keyboard_impl = {
+    .name = "tgui-keyboard",
+};
+
+struct wlr_backend *wlr_tgui_backend_create(struct wl_event_loop *loop) {
+    wlr_log(WLR_INFO, "Creating Termux:GUI backend");
+
+    struct wlr_tgui_backend *backend = calloc(1, sizeof(*backend));
+    if (!backend) {
+        wlr_log(WLR_ERROR, "Failed to allocate wlr_tgui_backend");
+        return NULL;
+    }
+    wlr_backend_init(&backend->backend, &backend_impl);
+
+    backend->loop = loop;
+    backend->tgui_event_fd = eventfd(0, EFD_CLOEXEC | EFD_NONBLOCK | EFD_SEMAPHORE);
+
+    if (tgui_connection_create(&backend->conn)) {
+        wlr_log(WLR_ERROR, "Failed to create tgui_connection");
+        wlr_backend_finish(&backend->backend);
+        free(backend);
+        return NULL;
+    }
+    backend->allocator = wlr_tgui_allocator_create(backend);
+
+    wlr_pointer_init(&backend->pointer, &tgui_pointer_impl, "tgui-pointer");
+    wlr_keyboard_init(&backend->keyboard, &tgui_keyboard_impl, "tgui-keyboard");
+
+    wl_list_init(&backend->outputs);
+
+    backend->event_loop_destroy.notify = handle_event_loop_destroy;
+    wl_event_loop_add_destroy_listener(loop, &backend->event_loop_destroy);
+
+    uint32_t events = WL_EVENT_READABLE | WL_EVENT_ERROR | WL_EVENT_HANGUP;
+    backend->tgui_event_source = wl_event_loop_add_fd(backend->loop, backend->tgui_event_fd,
+                                                      events, handle_tgui_event, backend);
+
+    wlr_queue_init(&backend->event_queue);
+    pthread_create(&backend->tgui_event_thread, NULL, tgui_event_thread, backend);
+
+    return &backend->backend;
+}
+
+struct wlr_allocator *wlr_tgui_backend_get_allocator(struct wlr_tgui_backend *backend) {
+    return backend->allocator;
+}
+
+bool wlr_backend_is_tgui(struct wlr_backend *backend) { return backend->impl == &backend_impl; }

x11-packages/wlroots/src/include/backend/termuxgui.h
@@ -1,0 +1,210 @@
+#ifndef BACKEND_TERMUXGUI_H
+#define BACKEND_TERMUXGUI_H
+
+#include <android/hardware_buffer.h>
+#include <assert.h>
+#include <pthread.h>
+#include <termuxgui/termuxgui.h>
+
+#include <wlr/backend/interface.h>
+#include <wlr/backend/termuxgui.h>
+#include <wlr/interfaces/wlr_buffer.h>
+#include <wlr/interfaces/wlr_keyboard.h>
+#include <wlr/interfaces/wlr_output.h>
+#include <wlr/interfaces/wlr_pointer.h>
+#include <wlr/render/allocator.h>
+#include <wlr/types/wlr_output_layer.h>
+#include <wlr/util/log.h>
+
+#define DEFAULT_REFRESH (60 * 1000) // 60 Hz
+
+#define TRY_LOG(func, ...)                                                                       \
+    do {                                                                                         \
+        tgui_err ret = func(__VA_ARGS__);                                                        \
+        if (ret != TGUI_ERR_OK) {                                                                \
+            wlr_log(WLR_ERROR, #func " failed: %s", TGUI_ERR_TO_STR(ret));                       \
+        }                                                                                        \
+    } while (0)
+
+static const char *TGUI_ERR_STR[] = {
+    [TGUI_ERR_SYSTEM] = "TGUI_ERR_SYSTEM",
+    [TGUI_ERR_CONNECTION_LOST] = "TGUI_ERR_CONNECTION_LOST",
+    [TGUI_ERR_ACTIVITY_DESTROYED] = "TGUI_ERR_ACTIVITY_DESTROYED",
+    [TGUI_ERR_MESSAGE] = "TGUI_ERR_MESSAGE",
+    [TGUI_ERR_NOMEM] = "TGUI_ERR_NOMEM",
+    [TGUI_ERR_EXCEPTION] = "TGUI_ERR_EXCEPTION",
+    [TGUI_ERR_VIEW_INVALID] = "TGUI_ERR_VIEW_INVALID",
+    [TGUI_ERR_API_LEVEL] = "TGUI_ERR_API_LEVEL",
+};
+
+static inline const char *TGUI_ERR_TO_STR(tgui_err err) {
+    if (err > TGUI_ERR_API_LEVEL) {
+        return "TGUI_ERR_UNKNOWN";
+    }
+    return TGUI_ERR_STR[err];
+}
+
+typedef struct native_handle {
+    int version; /* sizeof(native_handle_t) */
+    int numFds;  /* number of file-descriptors at &data[0] */
+    int numInts; /* number of ints at &data[numFds] */
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wzero-length-array"
+#endif
+    int data[0]; /* numFds + numInts ints */
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#endif
+} native_handle_t;
+
+struct wlr_queue {
+    struct wl_list base;
+    int length;
+    pthread_cond_t cond;
+    pthread_mutex_t mutex;
+};
+
+static inline void wlr_queue_init(struct wlr_queue *queue) {
+    queue->length = 0;
+    wl_list_init(&queue->base);
+    pthread_cond_init(&queue->cond, NULL);
+    pthread_mutex_init(&queue->mutex, NULL);
+}
+
+static inline void wlr_queue_destroy(struct wlr_queue *queue) {
+    pthread_cond_destroy(&queue->cond);
+    pthread_mutex_destroy(&queue->mutex);
+}
+
+static inline struct wl_list *wlr_queue_pull(struct wlr_queue *queue, bool nonblock) {
+    pthread_mutex_lock(&queue->mutex);
+
+    if (queue->length == 0) {
+        if (nonblock) {
+            pthread_mutex_unlock(&queue->mutex);
+            return NULL;
+        }
+        pthread_cond_wait(&queue->cond, &queue->mutex);
+    }
+    assert(queue->length > 0);
+
+    queue->length--;
+    struct wl_list *elm = queue->base.prev;
+    wl_list_remove(elm);
+
+    pthread_mutex_unlock(&queue->mutex);
+    return elm;
+}
+
+static inline void wlr_queue_push(struct wlr_queue *queue, struct wl_list *elm) {
+    pthread_mutex_lock(&queue->mutex);
+    if (wl_list_empty(&queue->base)) {
+        pthread_cond_signal(&queue->cond);
+    }
+    queue->length++;
+    wl_list_insert(&queue->base, elm);
+    pthread_mutex_unlock(&queue->mutex);
+}
+
+static inline int wlr_queue_length(struct wlr_queue *queue) {
+    pthread_mutex_lock(&queue->mutex);
+    int ret = queue->length;
+    pthread_mutex_unlock(&queue->mutex);
+    return ret;
+}
+
+struct wlr_tgui_backend {
+    struct wlr_backend backend;
+    struct wl_event_loop *loop;
+    struct wlr_allocator *allocator;
+
+    struct wlr_pointer pointer;
+    struct wlr_keyboard keyboard;
+
+    size_t requested_outputs;
+    struct wl_list outputs;
+    struct wl_listener event_loop_destroy;
+    bool started;
+
+    tgui_connection conn;
+    struct wlr_queue event_queue;
+    int tgui_event_fd;
+    pthread_t tgui_event_thread;
+    struct wl_event_source *tgui_event_source;
+};
+
+struct wlr_tgui_allocator {
+    struct wlr_allocator wlr_allocator;
+    tgui_connection conn;
+};
+
+struct wlr_tgui_buffer {
+    struct wlr_buffer wlr_buffer;
+
+    void *data;
+    uint32_t format;
+    tgui_connection conn;
+    tgui_hardware_buffer buffer;
+    AHardwareBuffer_Desc desc;
+    struct wl_list link;
+    struct wlr_dmabuf_attributes dmabuf;
+
+    void *dlhandle;
+    int (*lock)(AHardwareBuffer *buffer,
+                uint64_t usage,
+                int32_t fence,
+                const ARect *rect,
+                void **outVirtualAddress);
+    int (*unlock)(AHardwareBuffer *buffer, int32_t *fence);
+    void (*describe)(const AHardwareBuffer *buffer, AHardwareBuffer_Desc *outDesc);
+    const native_handle_t *(*getNativeHandle)(const AHardwareBuffer *buffer);
+};
+
+struct wlr_tgui_output {
+    struct wlr_output wlr_output;
+
+    struct wlr_tgui_backend *backend;
+    struct wl_list link;
+
+    tgui_activity activity;
+    tgui_view surface;
+    bool foreground;
+
+    struct wlr_queue present_queue;
+    struct wlr_queue idle_queue;
+    bool present_thread_run;
+    pthread_t present_thread;
+    int present_complete_fd;
+    struct wl_event_source *present_complete_source;
+
+    struct {
+        int id, max;
+        double x, y;
+        bool moved, down;
+        uint64_t time_ms;
+    } touch_pointer;
+
+    double cursor_x, cursor_y;
+};
+
+struct wlr_tgui_event {
+    tgui_event e;
+    struct wl_list link;
+};
+
+struct wlr_tgui_backend *tgui_backend_from_backend(struct wlr_backend *wlr_backend);
+
+struct wlr_allocator *wlr_tgui_allocator_create(struct wlr_tgui_backend *backend);
+
+struct wlr_allocator *wlr_tgui_backend_get_allocator(struct wlr_tgui_backend *backend);
+
+struct wlr_tgui_buffer *tgui_buffer_from_buffer(struct wlr_buffer *wlr_buffer);
+
+int handle_activity_event(tgui_event *e, struct wlr_tgui_output *output);
+
+void handle_touch_event(tgui_event *e, struct wlr_tgui_output *output, uint64_t time_ms);
+
+void handle_keyboard_event(tgui_event *e, struct wlr_tgui_output *output, uint64_t time_ms);
+
+#endif

x11-packages/wlroots/src/include/wlr/backend/termuxgui.h
@@ -1,0 +1,31 @@
+/*
+ * This an unstable interface of wlroots. No guarantees are made regarding the
+ * future consistency of this API.
+ */
+#ifndef WLR_USE_UNSTABLE
+#error "Add -DWLR_USE_UNSTABLE to enable unstable wlroots features"
+#endif
+
+#ifndef WLR_BACKEND_TERMUXGUI_H
+#define WLR_BACKEND_TERMUXGUI_H
+
+#include <wlr/backend.h>
+#include <wlr/types/wlr_output.h>
+
+/**
+ * Creates a Termux:GUI backend, and connection to the Termux:GUI plugin.
+ * A Termux:GUI backend has no outputs or inputs by default.
+ */
+struct wlr_backend *wlr_tgui_backend_create(struct wl_event_loop *loop);
+/**
+ * Create a new Termux:GUI output.
+ *
+ * Will use Termux:GUI plugin to create Activity and SurfaceView, the buffers presented
+ * on the output is displayed to the SurfaceView.
+ */
+struct wlr_output *wlr_tgui_output_create(struct wlr_backend *backend);
+
+bool wlr_backend_is_tgui(struct wlr_backend *backend);
+bool wlr_output_is_tgui(struct wlr_output *output);
+
+#endif